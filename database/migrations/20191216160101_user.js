f0e66f24cf8a3438a26ee1290f43cca15771bd89
ddelfaus <ddelfaus@gmail.com>
2019-12-16 16:55:59 -0500

    added roles like guided project

--- database/migrations/20191216160101_user.js
+++ database/migrations/20191216160101_user.js
@@ -11,15 +11,30 @@ exports.up = function(knex) {
   
         tbl.string("password", 128).notNullable();
   
-
+        tbl
+        .integer("role_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("roles")
+        .onUpdate("CASCADE") 
+        .onDelete("RESTRICT"); 
     })
 
+    .createTable("roles", tbl => {
+        tbl.increments();
 
+        tbl
+            .string("name", 155)
+            .notNullable()
+            .unique()
+    
+    })
   };
   
   exports.down = function(knex) {
     return knex.schema
     .dropTableIfExists("users")
- 
+   .dropTableIfExists("roles");
   };
   
