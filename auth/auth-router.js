fadfce3e935eb59e5f16760155555632936b7ab3
ddelfaus <ddelfaus@gmail.com>
2019-12-17 15:53:11 -0500

    added logout/saved sessions/protected access

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -28,7 +28,8 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-    
+        
+        req.session.user = user;
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
@@ -38,5 +39,24 @@ router.post("/login", (req, res) => {
       res.status(500).json(error);
     });
 });
+
+
+router.get("/logout", (req, res) => {
+  if (req.session) {
+    req.session.destroy(error => {
+      if (error) {
+        res.status(500).json({
+          message:
+            "logged out",
+        });
+      } else {
+        res.status(200).json({ message: "logged out" });
+      }
+    });
+  } else {
+    res.status(200).end();
+  }
+});
+
 
 module.exports = router;
